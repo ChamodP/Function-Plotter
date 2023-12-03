7c0ff93776cf26413a01a150ef629db7827ee862
Your Name <you@example.com>
2023-12-03 18:54:18 +0530

    completed all functionalities and basic CSS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,36 @@
-function handleSubmit(){
-    console.log(hello);
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  let right = document.querySelector(".right");
+
+  let parameters = [];
+  let xMin = document.getElementById("xMin").value;
+  let xMax = document.getElementById("xMax").value;
+  let yMin = document.getElementById("yMin").value;
+  let yMax = document.getElementById("yMax").value;
+
+  let color = document.getElementById("color").value;
+
+  let fn = document.getElementById("function").value;
+
+  parameters = {
+    target: "#functionPlot",
+    data: [
+      {
+        fn: fn,
+        color: color,
+      },
+    ],
+    grid: true,
+    yAxis: { domain: [yMin, yMax] },
+    xAxis: { domain: [xMin, xMax] },
+  };
+
+  functionPlot(parameters);
+
+  right.style.border = "solid 2px rgb(41, 39, 39)";
+  right.style.backgroundColor = "white";
+  right.style.padding = "30px";
+  right.style.borderRadius = "4px";
+  right.style.justifyContent = "center";
 }
